7f9d6cc6180389acc7f118f56a3e76cd981d0244
Paris <203000868+pdunmire-pcd@users.noreply.github.com>
2026-02-24 14:57:30 -0800

    fixed error in array & started the foundation for the confirmation page

--- app.js
+++ app.js
@@ -11,40 +11,36 @@ app.use(express.static('public'));
 
 const orders = [];
 
-//view engine will noe recognize ejs files and render them when requested
+// View engine will now recognize ejs files and render them when requested
 app.set('view engine', 'ejs');
 
 // Middleware to parse URL-encoded data from the request body
 app.use(express.urlencoded({ extended: true }));
 
 // Define a default "route" ('/')
-// req: contains information about the incoming request
-// res: allows us to send back a response to the client 
 app.get('/', (req, res) => {
     res.render('home');
 });
 
-
 app.post('/submit-order', (req, res) => {
-    
-    // Create a JSON object to store the order data
-    const orders = {
+
+    // Create an object to store the order data
+    const order = {
         name: req.body.name,
         email: req.body.email,
-        orderDetails: req.body.order-details,
-        toppings: req.body.topping ? req.body.topping : "none",
-        coneLegend: req.body.cone-legend,
         flavor: req.body.flavor,
+        cone: req.body.method,
+        toppings: req.body['topping[]'] ? [].concat(req.body['topping[]']).join(', ') : 'None',
+        comment: req.body.comment,
         timestamp: new Date()
     };
 
     // Add order object to orders array
-    orders.push(orders);
-    res.render('confirmation', { orders: orders });
+    orders.push(order);
+    res.render('confirmation', { order });
 });
 
 // Start the server and listen on the specified port
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
 });
-
