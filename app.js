a28b579337a20965bc62554696ba41f797d6ce52
Nadia <239692631+nadia5129@users.noreply.github.com>
2026-02-24 16:05:59 -0800

    port

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import express from 'express';
 const app = express();
 
 // Define the port number where our server will listen
-const PORT = 3007;
+const PORT = 3005;
 
 app.use(express.static('public'));
 
